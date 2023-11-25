4791b42883520381dd24392f5d49cab54b41e21c
vishnukumar <vishnu.kumar@digio.in>
2023-11-25 23:57:57 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-lg underline">hello there!</h1>
+    </>
+  );
+}
+
+export default App;
